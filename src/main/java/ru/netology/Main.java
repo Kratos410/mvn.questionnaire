5f23bcb0e446010669690f47cc8e9c7eff4a55f3
Kratos410 <fanur.tt64@gmail.com>
2023-01-29 13:48:35 +0300

    code editing

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,18 +3,19 @@ package ru.netology;
 import ru.netology.service.FormDate;
 import ru.netology.service.Post;
 
-public class Main {  public static void main(String[] args) {
-    Post post = new Post();
-    post.surname = "Иванов";
-    post.name = "Иван";
-    post.patronymic = "Иванович";
-    post.passport = "4444 № 44444444";
-    post.phone = "+7 (999)-999-99-99";
-    post.subscription = true;
-    post.birthday = new FormDate();
-    post.birthday.day= 13;
-    post.birthday.month= 6;
-    post.birthday.year= 1999;
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.surname = "Иванов";
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.passport = "4444 № 44444444";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
 
-}
+    }
 }
--- src/main/java/ru/netology/service/Post.java
+++ src/main/java/ru/netology/service/Post.java
@@ -3,10 +3,10 @@ package ru.netology.service;
 
 public class Post {
     public String name; // поле для имени
-    public  String passport; // поле для серии и номера паспорта
+    public String passport; // поле для серии и номера паспорта
     public String patronymic; // поле для отчества
     public String phone; //поле дл заполнение номера телефона
     public String surname; //поле для заполнения фамилии
-    public  Boolean subscription; // поле для заполнения статуса подписки
-   public FormDate birthday; // поле для заполнения даты рождения
+    public Boolean subscription; // поле для заполнения статуса подписки
+    public FormDate birthday; // поле для заполнения даты рождения
 }
